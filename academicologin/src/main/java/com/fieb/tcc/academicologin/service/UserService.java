d39ca259ab67249daf5b3bf2ee4e4d7cc5fd2fd2
Abner <abner.sol.pereira@gmail.com>
2023-03-20 22:50:15 -0300

    add login e home

---
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/service/UserService.java
@@ -0,0 +1,15 @@
+package com.fieb.tcc.academicologin.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.fieb.tcc.academicologin.model.User;
+import com.fieb.tcc.academicologin.web.dto.UserDto;
+
+public interface UserService extends UserDetailsService {
+
+	User save(UserDto UserDto);
+
+	User findByEmail(UserDto UserDto);
+
+	User update(User user);
+}
---
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/service/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.fieb.tcc.academicologin.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.fieb.tcc.academicologin.model.Role;
+import com.fieb.tcc.academicologin.model.User;
+import com.fieb.tcc.academicologin.repository.UserRepository;
+import com.fieb.tcc.academicologin.web.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepository.findByEmail(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("Invalid username or password");
+		}
+
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				mapRolesToAuthorities(user.getRoles()));
+	}
+
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+	}
+
+	@Override
+	public User save(UserDto UserDto) {
+		User user = new User(UserDto.getFirstName(), UserDto.getLastName(), UserDto.getEmail(),
+				passwordEncoder.encode(UserDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User findByEmail(UserDto UserDto) {
+		return userRepository.findByEmail(UserDto.getEmail());
+	}
+
+	@Override
+	public User update(User user) {
+		return null;
+	}
+
+}
